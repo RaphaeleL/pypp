c7f4f8cbf4a5eca2607fb6f89930fb8d7b4fbbaa
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-01-07 06:38:52 +0100

    Adjustments for Linux

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 echo "Time taken for 1000x1000 matrix multiplication in:"
 
 echo -n '- C                 '
-make -C basic_c -B
+make -C basic_c -B --no-print-directory
 
 echo -n '- C with ctypes     '
-make -C ctypes -B
+make -C ctypes -B --no-print-directory
 
 echo -n '- C with Python.h   '
-make -C py_header -B
+make -C py_header -B --no-print-directory
 
 echo -n '- Python3           '
-make -C basic_py -B
+make -C basic_py -B --no-print-directory
